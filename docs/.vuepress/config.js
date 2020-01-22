500a24daccee20eb0c032d290c36995317dbe91c
John Braun <johnbraun@pm.me>
2020-01-22 20:34:48 +0100

    Add favicons

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,17 @@
 module.exports = {
   title: 'Laravel Package Development',
   description: 'A central place to learn how to create packages from scratch.',
+  head: [
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/assets/favicons/apple-touch-icon.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/assets/favicons/favicon-32x32.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/assets/favicons/favicon-16x16.png"}],
+    ['link', { rel: "manifest", href: "/assets/favicons/site.webmanifest"}],
+    ['link', { rel: "mask-icon", href: "/assets/favicons/safari-pinned-tab.svg", color: "#3a0839"}],
+    ['link', { rel: "shortcut icon", href: "/assets/favicons/favicon.ico"}],
+    ['meta', { name: "msapplication-TileColor", content: "#3a0839"}],
+    ['meta', { name: "msapplication-config", content: "/assets/favicons/browserconfig.xml"}],
+    ['meta', { name: "theme-color", content: "#ffffff"}],
+  ],
   themeConfig: {
     logo: '/laravel-package-logo.png',
     nav: [
